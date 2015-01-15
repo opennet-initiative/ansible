b3301c9839ebe94e861ffe1007dbae3658dd3039
opennet-Konfigurationsverwaltung <ansible@heartofgold.opennet-initiative.de>
2015-01-15 01:30:00 +0100

    ein bissche kommentieren; iperf installieren

--- roles/ugw-server/files/openvpn/opennet_users/tls_verify.sh
+++ roles/ugw-server/files/openvpn/opennet_users/tls_verify.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#
+# Speicherung aller verwendeten Zertifikate zur Analyse alter/neuer Zertifikate
+#
+# NICHT MEHR IN VERWENDUNG
+
 filename=$(echo $2 | awk 'BEGIN{RS="[/\\;.?$]";FS="="} {printf $2"_"} END{print".crt"}')
 cp $peer_cert /etc/openvpn/opennet_users/certs/${untrusted_ip}$filename
 
