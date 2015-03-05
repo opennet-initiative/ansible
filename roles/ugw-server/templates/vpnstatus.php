783899265405e26a7afebf985627ddcbea375425
opennet-Konfigurationsverwaltung <ansible@heartofgold.opennet-initiative.de>
2015-03-05 23:29:40 +0100

    "ansible_hostname" durch manuell gesetzten "short_name" ersetzt

--- roles/ugw-server/templates/vpnstatus.php
+++ roles/ugw-server/templates/vpnstatus.php
@@ -1,11 +1,11 @@
 <?php
 	// {{ ansible_managed }}
 
-	$vpn_name = "{{ ansible_hostname }}.on User VPN";
+	$vpn_name = "{{ short_hostname }}.on User VPN";
 	$vpn_port = 7506;
         include 'openvpn_clients.php';
 
-	$vpn_name = "{{ ansible_hostname }}.on UGW VPN";
+	$vpn_name = "{{ short_hostname }}.on UGW VPN";
 	$vpn_port = 7505;
         include 'openvpn_clients.php';
 ?>
