a29f77bcde2d0c50654bdbea1c6e427328985261
Lars Kruse <devel@sumpfralle.de>
2017-12-30 19:45:20 +0100

    basic-server: mehr Shell-Einstellungen zur History

--- roles/basic-server/files/profile.d/on-basic-server.sh
+++ roles/basic-server/files/profile.d/on-basic-server.sh
@@ -6,3 +6,9 @@ if [ -n "$SSH_USER" ]; then
 	logger -ip auth.notice -t sshd "Accepted publickey for $SSH_USER"
 	export HISTFILE="$HOME/.history_$SSH_USER"
 fi
+
+# doppelte Eintraege ignorieren
+export HISTIGNORE=\&
+
+# laengere History
+export HISTSIZE=10000
