08cb6a919262e1803d913e205796650723880c12
Lars Kruse <devel@sumpfralle.de>
2017-12-30 19:30:49 +0100

    History-Separierung (via SSH_USER) zusammengefasst
    nun wir sie auch auf nicht-root-Nutzer angewandt, falls die
    Umgebungsvariable "SSH_USER" gesetzt via ssh-Schlüssel gesetzt wurde

---
+++ roles/basic-server/files/profile.d/on-basic-server.sh
@@ -0,0 +1,8 @@
+# History-Eintraege mit Zeitstempel versehen
+export HISTTIMEFORMAT='%F %T '
+
+# separate History-Datei pro Nutzer
+if [ -n "$SSH_USER" ]; then
+	logger -ip auth.notice -t sshd "Accepted publickey for $SSH_USER"
+	export HISTFILE="$HOME/.history_$SSH_USER"
+fi
