e53d23fa13c57bce16cf17c7a48f589abb227b7a
Martin Garbe <monomartin@opennet-initiative.de>
2017-12-31 11:48:54 +0100

    fixed bugs in manual-update-git-files-from-current-installation.sh

--- roles/mitgliederverwaltung-server/manual-update-git-files-from-current-installation.sh
+++ roles/mitgliederverwaltung-server/manual-update-git-files-from-current-installation.sh
@@ -8,10 +8,10 @@
 ssh root@yurika.on-i.de ls -l /var/www/mitgliederverwaltung
 
 #Hole individuelle Opennet Makros und kopiere ins git. So kann es eingecheckt werden, falls etwas neu ist.
-scp root@yurika.on-i.de:/var/lib/mitgliederverwaltung/data/plugin/macro/Opennet* files/macro/
+scp root@yurika.on-i.de:/var/lib/mitgliederverwaltung/data/plugin/macro/Opennet*py files/macro/
 
 #Hole apache config und kopieren ins git
 scp root@yurika.on-i.de:/etc/apache2/sites-available/wiki-mitgliederverwaltung.conf templates
-sed -i 's/\moin_static[0-9]+\/{{ moinmoin_version }}/' templates/wiki-mitgliederverwaltung.conf
+sed -ie 's/moin_static[0-9]*/moin_static{{ moinmoin_version }}/' templates/wiki-mitgliederverwaltung.conf
 
 echo "Now you should exec 'git diff' and check the diff."
