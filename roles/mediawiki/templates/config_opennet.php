6d1d47cd358bc10a4ba75ce70bde420fb41a5dbe
Mathias Mahnke <ap27@opennet-initiative.de>
2019-02-15 06:01:31 +0100

    mediawiki: add mediawiki update handler; move to short URLs

--- roles/mediawiki/templates/config_opennet.php
+++ roles/mediawiki/templates/config_opennet.php
@@ -68,4 +68,14 @@ $wgVectorResponsive = true;
 # Favicon
 $wgFavicon = "$wgResourceBasePath/images/favicon.ico";
 
+# Short URLs
+# based on https://shorturls.redwerks.org/
+## The URL base path to the directory containing the wiki;
+## defaults for all runtime URL paths are based off of this.
+## For more information on customizing the URLs please see:
+## http://www.mediawiki.org/wiki/Manual:Short_URL
+$wgScriptExtension = ".php";
+$wgArticlePath = "/$1";
+$wgUsePathInfo = true;
+
 ?>
