d28b610af6b650755e8ff754523a69e66aa68725
Mathias Mahnke <ap27@opennet-initiative.de>
2019-02-17 07:54:35 +0100

    apache2-server: include reweite rules for mediawiki short urls / allow php custom upload sizes; mediawiki: improve extensions

--- roles/mediawiki/templates/config_opennet.php
+++ roles/mediawiki/templates/config_opennet.php
@@ -24,8 +24,13 @@ wfLoadExtension( '{{ item }}' );
 {% endfor %}
 
 # TODO: some REL1_27 extension does not support wfLoadExtension yet
-require_once "{{ mediawiki_path_lib}}/extensions/ContactPage/ContactPage.php";
-require_once "{{ mediawiki_path_lib}}/extensions/TitleKey/TitleKey.php";
+require_once ( '{{ mediawiki_path_lib}}/extensions/ContactPage/ContactPage.php' );
+require_once ( '{{ mediawiki_path_lib}}/extensions/TitleKey/TitleKey.php' );
+require_once ( '{{ mediawiki_path_lib}}/extensions/NoTitle/NoTitle.php' );
+require_once ( '{{ mediawiki_path_lib}}/extensions/UserFunctions/UserFunctions.php' );
+
+# TODO: third party extension, other git
+wfLoadExtension( 'EmbedVideo' );
 
 ## 
 ## Configure Extensions
@@ -40,20 +45,42 @@ $arr = array (
 foreach ( $arr as $key => $value ) {
   $wgCaptchaQuestions[] = array( 'question' => $key, 'answer' => $value );
 }
+$wgEmailConfirmToEdit = true;
 
 # Config MobileFrontend Extension
 $wgMFAutodetectMobileView = true;
 
+# Config User Functions (depends Parser Functions)
+# needed for MediaWiki:Sidebar customization
+$wgEnableSidebarCache = false;
+$wgUFAllowedNamespaces[NS_MAIN] = true;
+
+# Contact Page
+# a valid WikiUser is required, please create before
+# TODO: OpennetContactForm scripting?
+$wgContactConfig['default'] = array(
+  'RecipientUser' => 'Mathias', 
+  'SenderName' => $wgSitename . ' Kontaktformular', 
+  'SenderEmail' => null, // Defaults to $wgPasswordSender
+  'RequireDetails' => true, 
+  'IncludeIP' => false, 
+  'AdditionalFields' => array(
+    'Text' => array(
+      'label-message' => 'emailmessage',
+      'type' => 'textarea',
+      'rows' => 15,
+      'required' => true, 
+    ),
+  ),
+  'DisplayFormat' => 'table',  
+  'RLModules' => array(),  
+  'RLStyleModules' => array()
+);
+$wgCaptchaTriggers['contactpage'] = true;
+
 ##
 ## Other Configuration
 ## 
-
-# Contact Page
-$wgContactUser = 'OpennetContactForm';
-$wgContactSender = $wgPasswordSender;
-$wgContactSenderName = $wgSitename . ' Kontaktformular';
-$wgCaptchaTriggers['contactpage'] = true;
-$wgContactRequireAll = true;
 
 # File Upload
 {% for item in mediawiki_upload_filetypes %}
@@ -61,7 +88,7 @@ $wgFileExtensions[] = "{{ item }}";
 {% endfor %}
 
 # Vector Skin
-$wgVectorUseSimpleSearch = true;
+#$wgVectorUseSimpleSearch = true;
 $wgVectorUseIconWatch = true;
 $wgVectorResponsive = true;
 
@@ -77,5 +104,27 @@ $wgFavicon = "$wgResourceBasePath/images/favicon.ico";
 $wgScriptExtension = ".php";
 $wgArticlePath = "/$1";
 $wgUsePathInfo = true;
+
+# Allow Pagetitle Overrive
+# used for Main_Page
+$wgRestrictDisplayTitle = false; 
+
+# Hide Page History
+function efAddSkinStyles(OutputPage &$out, Skin &$skin) {
+  if(!$skin->getUser()->isLoggedIn()) {
+    $out->addInlineStyle('#ca-view { display:none; }');
+    $out->addInlineStyle('#ca-viewsource { display:none; }');
+    $out->addInlineStyle('#ca-history { display:none; }');
+    $out->addInlineStyle('#mw-mf-last-modified { display:none; }');
+    $out->addInlineStyle('#footer-info { display: none; }');
+    $out->addInlineStyle('#p-tb { display: none; }');
+  }
+  return true;
+}
+$wgHooks['BeforePageDisplay'][] = 'efAddSkinStyles';
+
+# Html / Embedded iFrames, needed? 
+#$wgRawHtml = true;
+#$wgAllowExternalImages = true;
 
 ?>
