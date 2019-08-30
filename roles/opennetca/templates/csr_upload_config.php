aa3053764d6ab3da9b8166537789c118dc67aa8f
Mathias Mahnke <ap27@opennet-initiative.de>
2019-08-30 13:37:15 +0200

    opennetca: implement workaround to allow cert-based login, use separate vhost for internal access

--- roles/opennetca/templates/csr_upload_config.php
+++ roles/opennetca/templates/csr_upload_config.php
@@ -20,5 +20,12 @@ $mailto = "Opennet CSR Team <{{ opennetca_mail_csrto }}>";
 $mailfrom = "Opennet CA <opennetca@opennet-initiative.de>";
 $mailsubject = "Opennet CA (upload): Signing Request / Zertifikatsanfrage";
 $mailfooter = "-- \r\nOpennet Initiative e.V.\r\nhttps://www.opennet-initiative.de\r\nCA Status: {{ opennetca_url }}";
-$approveurl = "{{ opennetca_url }}/internal/csr_approve.php?";
+// Workaround, 2019-08-30, mixed cert and non-cert HTTPS site 
+// currently not possible in Apache / Debian "buster" and current browser
+// * https://bugzilla.redhat.com/show_bug.cgi?id=1651623
+// * https://github.com/ruby/openssl/issues/237
+// * https://bugs.chromium.org/p/chromium/issues/detail?id=911653
+$approveurl = "https://ca-internal.opennet-initiative.de/internal/csr_approve.php?";
+// $approveurl = "{{ opennetca_url }}/internal/csr_approve.php?";
+
 ?>
