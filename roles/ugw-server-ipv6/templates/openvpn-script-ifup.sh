30b0383e3d59af68e53ed31d63e940b6afefb990
Martin Garbe <monomartin@opennet-initiative.de>
2020-03-21 22:16:41 +0100

    - fix bug: do not restart OpenVPN while starting OpenVPN connection - restart dhpcd6 server

--- roles/ugw-server-ipv6/templates/openvpn-script-ifup.sh
+++ roles/ugw-server-ipv6/templates/openvpn-script-ifup.sh
@@ -6,5 +6,5 @@ ip addr add 2a0a:4580:1010:0002::1/64 dev {{ openvpn_users_l2_v6_interface }}
 ip link set dev tap-users-v6 up
 
 #restart services which rely on this interface
-systemctl restart openvpn@opennet_user_l2vpn_v6
+systemctl restart isc-dhcp-server
 systemctl restart radvd
