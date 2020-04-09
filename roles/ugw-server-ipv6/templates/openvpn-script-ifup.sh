2852d4c3d3c38d54da84eb984b51310baaa11069
Martin Garbe <monomartin@opennet-initiative.de>
2020-04-09 21:33:27 +0200

    fix bug: es muss bekannt sein, dass das Netz am tap-users-v6 angeschlossen ist. Sonst funktioniert Routing nicht.

--- roles/ugw-server-ipv6/templates/openvpn-script-ifup.sh
+++ roles/ugw-server-ipv6/templates/openvpn-script-ifup.sh
@@ -4,6 +4,8 @@
 ip addr add 2a0a:4580:1010:0002::1/64 dev {{ openvpn_users_l2_v6_interface }}
 #set IF up
 ip link set dev tap-users-v6 up
+#add routing for dhcpv6-pd range
+ip route add 2a0a:4580:1010:1000::/52 dev tap-users-v6
 
 #restart services which rely on this interface
 systemctl restart isc-dhcp-server
