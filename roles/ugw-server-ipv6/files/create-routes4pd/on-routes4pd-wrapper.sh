489f881dab11111f6d60b599f5116f23d4e353b3
Martin Garbe <monomartin@opennet-initiative.de>
2020-04-28 22:50:26 +0200

    start routing table update as result of DHCPv6 activities

---
+++ roles/ugw-server-ipv6/files/create-routes4pd/on-routes4pd-wrapper.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+PIDFILE=/var/run/on-routes4pd-wrapper.pid
+
+#
+#make sure that there is only one of this processes running.
+# this is needed because when using "on commit" hooks in ISC DHCPd6 then
+# this script is started quickly one after another and the python script then 
+# is raising errors.
+#
+if [ -f "$PIDFILE" ] ; then
+  PID=$(cat $PIDFILE)
+  RUNCMD=$(ps -q $PID -o comm | grep -v COMMAND)
+  if [ -z "$RUNCMD" ] ; then
+    #there is a PID file but there is no process running. delete PID file.
+    rm $PIDFILE
+    echo "Delete $PIDFILE because there is no running process."
+  fi
+fi
+
+if [ ! -f "$PIDFILE" ] ; then
+  /bin/sh -c "sleep 5 && /usr/local/bin/on-routes4pd.py -v 1 -l /var/log/create-routes4pd.log && rm $PIDFILE" &
+  PID=$!
+  echo $PID > "$PIDFILE"
+  echo "wrote: $PID > $PIDFILE"
+fi
+
