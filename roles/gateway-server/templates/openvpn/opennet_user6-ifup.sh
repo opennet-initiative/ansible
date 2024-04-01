62bbf8014d9517c308c8fe6d7e9d7e6f60c5432e
Martin Garbe <monomartin@opennet-initiative.de>
2024-04-01 23:16:31 +0200

    gateway-server: finalize config for openvpn-user6
    Also delete custom data-cipher config because we do not have old
    APs with old firmware connecting here.

--- roles/gateway-server/templates/openvpn/opennet_user6-ifup.sh
+++ roles/gateway-server/templates/openvpn/opennet_user6-ifup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #set IP on interface
-ip addr add 2a0a:4580:1010:0002::1/64 dev {{ openvpn_users_l2_v6_interface }}
-#set IF up
-ip link set dev tap-users-v6 up
+ip addr add 2a0a:4580:1010:0002::1/64 dev {{ openvpn_user6_interface }}
+#activate interface
+ip link set dev {{ openvpn_user6_interface }} up
 #add routing for dhcpv6-pd range
-ip route add 2a0a:4580:1010:1000::/52 dev tap-users-v6
+ip route add 2a0a:4580:1010:1000::/52 dev {{ openvpn_user6_interface }}
 
 #restart services which rely on this interface
 systemctl restart isc-dhcp-server
